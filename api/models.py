c29bd9d9a587401281dda3f4eb32d95bffd97d90
Cristobal Vidal Orquin <crvidorq@upv.edu.es>
2023-02-07 17:00:09 +0100

    Login creado, y configurado en API

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    username = models.CharField(max_length=50, primary_key=True)
     password = models.CharField(max_length=50)
 
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+class LoginView(APIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                 return Response({'error': 'Invalid login credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)             
