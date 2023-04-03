5a25e8f58942cc10603fc92509de3b43c1ea399e
Cristobal Vidal Orquin <crvidorq@upv.edu.es>
2023-04-03 14:45:38 +0200

    Revert "Cambio de BD"
    This reverts commit f55fbc328d57f7b33e6bb7a0f5b8bbed434fb4e1.

--- backend/settings.py
+++ backend/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tfgbd',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
