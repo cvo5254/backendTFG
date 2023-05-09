7c6d253ce4ecd1e3bdd30830bffe429c51a6621f
Cristobal Vidal Orquin <crvidorq@upv.edu.es>
2023-05-09 17:08:10 +0200

    Modelos de canales y emergencias añadidos

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Channel(models.Model):
+    nombre = models.CharField(max_length=255)
+    subscribers = models.ManyToManyField(CustomUser)
--- emergencies/models.py
+++ emergencies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from channels.models import Channel
+from users.models import CustomUser
 
-# Create your models here.
+class Emergency(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    report_date = models.DateTimeField()
+    publish_date = models.DateTimeField(blank=True, null=True)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, null=True, blank=True)
+    reporter = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
